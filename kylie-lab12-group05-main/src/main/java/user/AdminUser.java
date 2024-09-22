968f8ba6f8e895aa936658db6f5b9b19903291b8
Mohsin <104878156+msid8535@users.noreply.github.com>
2024-09-22 18:09:29 +1000

    Delete kylie-lab12-group05-main directory

--- kylie-lab12-group05-main/src/main/java/user/AdminUser.java
+++
@@ -1,9 +0,0 @@
-package user;
-
-public class AdminUser extends User {
-
-    public AdminUser(String username, String password) {
-        super(username, password, true);
-    }
-
-}
--- kylie-lab12-group05-main/src/main/java/user/NormalUser.java
+++
@@ -1,9 +0,0 @@
-package user;
-
-public class NormalUser extends User {
-
-    public NormalUser(String username, String password) {
-        super(username, password, false);
-    }
-
-}
